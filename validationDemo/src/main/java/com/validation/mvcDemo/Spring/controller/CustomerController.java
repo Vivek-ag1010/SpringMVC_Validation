6cdf174e8e085323f16119247dce807cd1634e25
Vivek <vivek.agrahari@xcelore.com>
2024-02-08 12:55:56 +0530

    Inspect the Binding Result object- Log in console

--- validationDemo/src/main/java/com/validation/mvcDemo/Spring/controller/CustomerController.java
+++ validationDemo/src/main/java/com/validation/mvcDemo/Spring/controller/CustomerController.java
@@ -32,7 +32,11 @@ public class CustomerController {
     @PostMapping("/processForm")
     public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult)
     {
+
         System.out.println("Last Name: |"+theCustomer.getLastName()+"|");
+        System.out.println("Binding results: "+theBindingResult.toString());
+        System.out.println("\n\n\n\n");
+
         if(theBindingResult.hasErrors()){
             return "customer-form";
         }
